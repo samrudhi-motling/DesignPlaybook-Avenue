307d1185c5c8c18d766f301320678a15b62d1075
nk-neha <115693111+nk-neha@users.noreply.github.com>
2025-02-24 14:25:12 +0530

    Delete avenue directory

--- avenue/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white text-center py-6 mt-10">
-      <p>&copy; 2025 BrandLyft. All Rights Reserved.</p>
-    </footer>
-  );
-}
